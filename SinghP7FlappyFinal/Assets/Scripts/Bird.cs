c34d98eefa79eaf94e3997cb9cd762ed51c88685
768650-cell <768650@student.dusd.net>
2025-12-09 15:30:50 -0800

    added project
    Add project add assite and add ground and sky

---
+++ SinghP7FlappyFinal/Assets/Scripts/Bird.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown (0))
+            {
+                rb2D.velocity = Victor2.zero;
+                rb2D.AddForce (new Vector2 (0, upForce));
+            }
+        }
+    }
+}
